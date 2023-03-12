1a9b1ff3a8326574721c400fbd284e9893dd1e51
Shfty <Shfty@users.noreply.github.com>
2023-03-12 03:43:05 +0000

    deploy: 4dee8b7338dd4b75c8d68af6d52dc9c688ff9800

--- rust_gpu_sdf/sidebar-items.js
+++ rust_gpu_sdf/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["default","Free-standing [`Default::default()`] invocation"]],"mod":[["operators","Types that modify a distance field."],["signed_distance_field","Types that describe signed distance fields."]],"type":[["D2",""],["D3",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["default","Free-standing [`Default::default()`] invocation"]],"mod":[["operators","Types that modify a distance field."],["prelude",""],["signed_distance_field","Types that describe signed distance fields."]],"type":[["D2",""],["D3",""]]};
---
+++ rust_gpu_sdf/signed_distance_field/attributes/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":[["color",""],["distance",""],["normal",""],["position",""],["tangent",""],["uv",""]]};
