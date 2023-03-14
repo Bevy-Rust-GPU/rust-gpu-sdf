cae8e7449fc53e5ba9bacee9a075b8fe447384c6
Shfty <Shfty@users.noreply.github.com>
2023-03-14 03:30:26 +0000

    deploy: f89e69c3587e087f17d9d30cf27e4e1353086c15

--- rust_gpu_sdf/operators/sided/sidebar-items.js
+++ rust_gpu_sdf/operators/sided/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["SidedOp",""]],"type":[["Sided",""]]};
+window.SIDEBAR_ITEMS = {"struct":[["SidedOp","Given an infinitely-thin surface, divide space into interior and exterior based on axis."]],"type":[["Sided",""]]};
