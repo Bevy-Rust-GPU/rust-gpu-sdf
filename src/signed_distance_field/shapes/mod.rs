8acc1b6a27311d47c463714e7712a8749aaba80e
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-03-15 06:12:30 +0000

    Split superellipsoid module into its own file

--- src/signed_distance_field/shapes/mod.rs
+++ src/signed_distance_field/shapes/mod.rs
@@ -5,45 +5,4 @@ pub mod octahedron;
 pub mod plane;
 pub mod squircle;
 pub mod superellipse;
-pub mod superellipsoid {
-    use rust_gpu_bridge::prelude::Vec3;
-    use type_fields::Field;
-
-    use crate::signed_distance_field::{attributes::distance::Distance, SignedDistanceField};
-
-    #[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Field)]
-    pub struct Superellipsoid {
-        pub e1: f32,
-        pub e2: f32,
-    }
-
-    impl Default for Superellipsoid {
-        fn default() -> Self {
-            Superellipsoid { e1: 1.0, e2: 1.0 }
-        }
-    }
-
-    impl SignedDistanceField<Vec3, Distance> for Superellipsoid {
-        fn evaluate(&self, p: Vec3) -> Distance {
-            let d = (p.x.abs().powf(self.e1) + p.y.abs().powf(self.e2)).powf(self.e2 / self.e1)
-                + p.z.abs().powf(self.e1);
-            let d = d - 1.0;
-            Distance(d)
-        }
-    }
-
-    #[cfg(test)]
-    pub mod test {
-        use rust_gpu_bridge::prelude::Vec3;
-
-        use crate::prelude::BoundChecker;
-
-        use super::Superellipsoid;
-
-        #[test]
-        #[should_panic]
-        fn test_superellipsoid() {
-            assert!(BoundChecker::<Vec3, Superellipsoid>::default().is_field())
-        }
-    }
-}
+pub mod superellipsoid;
