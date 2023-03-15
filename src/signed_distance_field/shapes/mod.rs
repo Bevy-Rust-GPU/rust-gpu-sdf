01a64076907ae1de715512e13ef6e49fc016ae29
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-03-15 05:49:51 +0000

    Implement superellipse

--- src/signed_distance_field/shapes/mod.rs
+++ src/signed_distance_field/shapes/mod.rs
@@ -4,26 +4,23 @@ pub mod composite;
 pub mod octahedron;
 pub mod plane;
 pub mod squircle;
-pub mod lame_curve {
-    use rust_gpu_bridge::prelude::Vec2;
+pub mod superellipse;
+pub mod superellipsoid {
+    use rust_gpu_bridge::prelude::Vec3;
     use type_fields::Field;
 
     use crate::signed_distance_field::{attributes::distance::Distance, SignedDistanceField};
 
-    #[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Field)]
-    pub struct LameCurve {
-        pub n: f32,
+    #[derive(Debug, Default, Copy, Clone, PartialEq, PartialOrd, Field)]
+    pub struct Superellipsoid {
+        pub e1: f32,
+        pub e2: f32,
     }
 
-    impl Default for LameCurve {
-        fn default() -> Self {
-            LameCurve { n: 1.0 }
-        }
-    }
-
-    impl SignedDistanceField<Vec2, Distance> for LameCurve {
-        fn evaluate(&self, p: Vec2) -> Distance {
-            let d = p.x.abs().powf(self.n) + p.y.abs().powf(self.n);
+    impl SignedDistanceField<Vec3, Distance> for Superellipsoid {
+        fn evaluate(&self, p: Vec3) -> Distance {
+            let d = (p.x.abs().powf(self.e1) + p.y.abs().powf(self.e2)).powf(self.e2 / self.e1)
+                + p.z.abs().powf(self.e1);
             let d = d - 1.0;
             Distance(d)
         }
@@ -31,16 +28,16 @@ pub mod lame_curve {
 
     #[cfg(test)]
     pub mod test {
-        use rust_gpu_bridge::prelude::Vec2;
+        use rust_gpu_bridge::prelude::Vec3;
 
         use crate::prelude::BoundChecker;
 
-        use super::LameCurve;
+        use super::Superellipsoid;
 
         #[test]
         #[should_panic]
-        fn test_lame_curve() {
-            assert!(BoundChecker::<Vec2, LameCurve>::default().is_field())
+        fn test_superellipsoid() {
+            assert!(BoundChecker::<Vec3, Superellipsoid>::default().is_field())
         }
     }
 }
---
+++ src/signed_distance_field/shapes/superellipse.rs
@@ -0,0 +1,39 @@
+use rust_gpu_bridge::prelude::Vec2;
+use type_fields::Field;
+
+use crate::signed_distance_field::{attributes::distance::Distance, SignedDistanceField};
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Field)]
+pub struct Superellipse {
+    pub n: f32,
+}
+
+impl Default for Superellipse {
+    fn default() -> Self {
+        Superellipse { n: 1.0 }
+    }
+}
+
+impl SignedDistanceField<Vec2, Distance> for Superellipse {
+    fn evaluate(&self, p: Vec2) -> Distance {
+        let d = p.x.abs().powf(self.n) + p.y.abs().powf(self.n);
+        let d = d - 1.0;
+        Distance(d)
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use rust_gpu_bridge::prelude::Vec2;
+
+    use crate::prelude::BoundChecker;
+
+    use super::Superellipse;
+
+    #[test]
+    #[should_panic]
+    fn test_lame_curve() {
+        assert!(BoundChecker::<Vec2, Superellipse>::default().is_field())
+    }
+}
+
